b65fb089c64ae7bd4f4ce5cb47d82b2ca604cdea
ewall <1054064180@qq.com>
2021-02-17 23:47:57 +0800

    add

--- index.js
+++ index.js
@@ -644,7 +644,7 @@ var merge = function (intervals) {
 https://leetcode-cn.com/problems/summary-ranges/
 
 给定一个无重复元素的有序整数数组 nums 。
-返回 恰好覆盖数组中所有数字 的 最小有序 区间范围列表。也就是说，nums 的每个元素都恰好被某个区间范围所覆盖，并且不存在属于某个范围但不属于 nums 的数字 x 。
+返回【恰好覆盖数组中所有数字】的【最小有序】区间范围列表。也就是说，nums 的每个元素都恰好被某个区间范围所覆盖，并且不存在属于某个范围但不属于 nums 的数字 x 。
 
 列表中的每个区间范围 [a,b] 应该按如下格式输出：
 
@@ -680,9 +680,10 @@ https://leetcode-cn.com/problems/summary-ranges/
 输入：nums = [0]
 输出：["0"]
 
-logs：02
+logs：03
 [✔️]2021.01.27
 [✔️]2021.02.02
+[✔️]2021.02.17
 */
 
 // 双指针。时间复杂度O(n)、空间复杂度O(n)
@@ -3841,9 +3842,10 @@ https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/
 
 输出: [1, 3, 9]
 
-logs：02
+logs：03
 [✔️]2021.02.01
 [✔️]2021.02.02
+[✔️]2021.02.17
 */
 // BFS 时间复杂度O(n)
 /**
