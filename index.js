59cb1020ecb2aad127a034e5a691ce6e7e01900a
ewall <1054064180@qq.com>
2021-03-11 13:05:10 +0800

    add

--- index.js
+++ index.js
@@ -5613,10 +5613,26 @@ var climbStairs = function (n) {
 
 /*
 【斐波那契数列】
-logs：01
+https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+
+示例 1：
+输入：n = 2
+输出：1
+
+示例 2：
+输入：n = 5
+输出：5
+
+logs：02
 [✔️]2021.03.06
+[✔️]2021.03.11
 */
-// 暴力递归。时间复杂度O(2^n)
+// 暴力递归。时间复杂度O(2^n)、空间复杂度O(n)
+// 自顶向下
 var fibonacci = function (n) {
   if (n <= 1) {
     return n;
@@ -5624,25 +5640,39 @@ var fibonacci = function (n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-// 记忆化搜索。时间复杂度O(n)
-// 加个缓存优化、自顶向下来思考解决问题
-var fibonacci = function (n, memo) {
+// 记忆化搜索。时间复杂度O(n)、空间复杂度O(n)
+// 自顶向下，加个缓存优化、自顶向下来思考解决问题。
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n, memo = []) {
   if (n <= 1) {
     return n;
   }
   // 如果此数没有缓存过
-  if (memo[n] === 0) {
-    memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+  if (!memo[n]) {
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
   }
   return memo[n];
 };
 
-// 自底向上进行递推。
-// var fibonacci = function (n, memo) {
-//   if (n <= 1) {
-//     return n;
-//   }
-// };
-//
-// -------divider-------
-//
+// DP。时间复杂度O(n)、空间复杂度O(n) 自底向上进行递推。
+var fib = function (n) {
+  let arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+};
+
+// 尾递归。时间复杂度O(n)、空间复杂度O(1）
+// 尾递归调用不需要保存外层函数的执行上下文，所以调用栈不会溢出。（其实就是相当于一个for循环）
+// https://ruanyifeng.com/blog/2015/04/tail-call.html
+function fib(n, a = 1, b = 1) {
+  if (n <= 0) return 0;
+  if (n === 1) return a;
+  return fib(n - 1, b, a + b);
+}
+
+// 通项公式法。（不展开、套数学公式即可） 时间复杂度O(1)
