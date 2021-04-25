96f07eb1289af86128dcb169a3ffe9840b64310f
ewall <1054064180@qq.com>
2021-04-25 22:45:49 +0800

    add

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 图、DFS、BFS
 动态规划、贪心算法
 排序、搜索算法
+LRU Cache
 */
 
 /* -------------------------- 数组、栈 ---------------------------*/
@@ -6287,7 +6288,7 @@ logs：03
 var coinChange = function (coins, amount) {
   // dp数组初始化为 amount+1, 因为凑成 amount 金额的硬币数最多只可能等于 amount（全用1元面值的硬币）
   // 所以初始化为 amount+1 就相当于初始化为正无穷，便于后续取最小值
-  let dp = new Array(amount + 1).fill(amount + 1); 
+  let dp = new Array(amount + 1).fill(amount + 1);
   dp[0] = 0; // dp：[0, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12] 每一项代表的是多少元
   for (let i = 1; i < dp.length; i++) {
     for (let j = 0; j < coins.length; j++) {
@@ -6456,3 +6457,63 @@ function fib(n, a = 1, b = 1) {
   if (n === 1) return a;
   return fib(n - 1, b, a + b); // 当函数return返回时，该函数的执行上下文就会从栈顶弹出
 }
+
+/* -------------------------- LRU Cache ---------------------------*/
+/*
+Least Recently Used Cache (最近最少使用)
+使用 Double LinkedList 双向链表来实现
+*/
+
+/*
+【LRU 缓存机制】
+
+运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+
+实现 LRUCache 类：
+LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+
+logs：01
+[✔️]2021.04.25
+*/
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.map = new Map();
+  this.capacity = capacity;
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (!this.map.has(key)) return -1;
+  const val = this.map.get(key);
+  this.map.delete(key);
+  this.map.set(key, val);
+  return val;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  this.map.delete(key);
+  this.map.set(key, value);
+  if (this.map.size > this.capacity) {
+    const firstItem = this.map.keys().next().value; // iterator可迭代器
+    this.map.delete(firstItem);
+  }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
