d9128aa62db5b8dfad64f21749f6a4acc0bc3e1a
ewall <1054064180@qq.com>
2021-06-06 23:24:00 +0800

    add

--- index.js
+++ index.js
@@ -5628,11 +5628,12 @@ https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
 输出: 0
 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
 
-logs：04
+logs：05
 [✔️]2021.03.30
 [✔️]2021.04.20
 [✔️]2021.05.08
 [✔️]2021.05.18
+[✔️]2021.06.06
 */
 // 动态规划。O(n*2)
 /**
@@ -7125,6 +7126,64 @@ var reverseWords = function (s) {
     }
   }
   return res + word;
+};
+
+//
+// -------divider-------
+//
+
+/*
+【翻转字符串里的单词】
+https://leetcode-cn.com/problems/reverse-words-in-a-string/
+
+给你一个字符串 s ，逐个翻转字符串中的所有 单词 。
+单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+请你返回一个翻转 s 中单词顺序并用单个空格相连的字符串。
+
+说明：
+输入字符串 s 可以在前面、后面或者单词间包含多余的空格。
+翻转后单词间应当仅用一个空格分隔。
+翻转后的字符串中不应包含额外的空格。
+ 
+示例 1：
+输入：s = "the sky is blue"
+输出："blue is sky the"
+
+示例 2：
+输入：s = "  hello world  "
+输出："world hello"
+解释：输入字符串可以在前面或者后面包含多余的空格，但是翻转后的字符不能包括。
+
+示例 3：
+输入：s = "a good   example"
+输出："example good a"
+解释：如果两个单词间有多余的空格，将翻转后单词间的空格减少到只含一个。
+
+logs：01
+2021.06.06
+*/
+var reverseWords = function (s) {
+  s = s.trim();
+  let res = '';
+  let word = '';
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') {
+      s[i - 1] !== ' ' && (res = s[i] + word + res);
+      word = '';
+    } else {
+      word += s[i];
+    }
+  }
+  return word + res;
+};
+
+// 使用额外数组
+var reverseWords = function (s) {
+  s = s.split(' ');
+  let res = s.filter(item => {
+    return item != '';
+  });
+  return res.reverse().join(' ');
 };
 
 //
