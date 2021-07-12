4357397406aa359e04bff3bec5b177ac4421f884
ewall <1054064180@qq.com>
2021-07-12 19:54:13 +0800

    add

--- index.js
+++ index.js
@@ -15,6 +15,70 @@ LRU Cache
 /* -------------------------- 数组、栈 ---------------------------*/
 
 /*
+【最长连续序列】
+https://leetcode-cn.com/problems/longest-consecutive-sequence/
+
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+进阶：你可以设计并实现时间复杂度为 O(n) 的解决方案吗？
+
+示例 1：
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+
+示例 2：
+输入：nums = [0,3,7,2,5,8,4,6,0,1]
+输出：9
+
+logs：01
+2021.07.10
+*/
+// 哈希表 时间复杂度O(n) 空间复杂度O(n)
+var longestConsecutive = function (nums) {
+  if (!nums || !nums.length) return 0;
+  let set = new Set(nums);
+  let max = 0;
+
+  for (let num of set) {
+    if (set.has(num - 1)) continue;
+
+    let currNum = num;
+    let currMax = 1;
+
+    while (set.has(currNum + 1)) {
+      currNum++;
+      currMax++;
+    }
+    max = Math.max(max, currMax);
+  }
+
+  return max;
+};
+
+// 是一种解法，但是不符合题意
+var longestConsecutive = function (nums) {
+  if (!nums || !nums.length) return 0;
+  nums.sort((a, b) => a - b);
+  let count = 1;
+  let res = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    } else if (nums[i] - nums[i - 1] === 1) {
+      count++;
+    } else {
+      res = Math.max(res, count);
+      count = 1;
+    }
+  }
+  return Math.max(res, count);
+};
+
+//
+// -------divider-------
+//
+
+/*
 【打乱数组 洗牌算法】
 https://leetcode-cn.com/problems/shuffle-an-array/
 给你一个整数数组 nums ，设计算法来打乱一个没有重复元素的数组。
